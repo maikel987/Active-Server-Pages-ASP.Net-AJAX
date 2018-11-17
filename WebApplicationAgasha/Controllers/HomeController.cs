95f0f1410700c64ec28c93c761f7f573cd2ffaec
Maikel <Maikelsitbon@gmail.com>
2018-11-17 21:24:05 +0200

    Add project files.

---
+++ WebApplicationAgasha/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationAgasha.Models;
+
+namespace WebApplicationAgasha.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [OutputCache(Duration = 3600)]
+        public ActionResult _ShowCity()
+        {
+            List<City> result;
+            using (MyDBEntities context = new MyDBEntities())
+            {
+                var cities = from c in context.City select c;
+                result = cities.ToList();
+            }
+
+            return PartialView(result);
+        }
+        [OutputCache(Duration = 3600)]
+        public ActionResult _ShowClient()
+        {
+            List<Client> result;
+            using (MyDBEntities context = new MyDBEntities())
+            {
+                var clients = from c in context.Client select c;
+                result = clients.ToList();
+            }
+
+            return PartialView(result);
+        }
+    }
+}
